0718aff80df6ba301292d8c3b7709dcc14b3dcad
Vignesh-Rathnakumar <113254753+Vignesh-Rathnakumar@users.noreply.github.com>
2023-08-28 18:16:31 +0530

    Update test_checksum.c

--- tests/test_checksum.c
+++ tests/test_checksum.c
@@ -1,22 +1,31 @@
-#include "crc.h"
+#include <stdio.h>
 #include <string.h>
-#include "unity.h"
 
-void test_checksum_over_examples(void) {
-  const char* examples[] = {
-    "$GPGSV,2,1,08,02,74,042,45,04,18,190,36,07,67,279,42,12,29,323,36*77",
-    "$GPGSV,2,2,08,15,30,050,47,19,09,158,,26,12,281,40,27,38,173,41*7B"
-  };
-  TEST_ASSERT_EQUAL(1, verify_checksum(examples[0], strlen(examples[0])));
-  TEST_ASSERT_EQUAL(1, verify_checksum(examples[1], strlen(examples[1])));
+// Calculate the checksum of an NMEA sentence
+unsigned char calculate_checksum(const char *sentence) {
+    unsigned char checksum = 0;
+    int i;
+
+    for (i = 0; sentence[i] != '*' && sentence[i] != '\0'; i++) {
+        checksum ^= sentence[i];
+    }
+
+    return checksum;
 }
 
-void setUp() {}
-void tearDown() {}
+// Verify the checksum of an NMEA sentence
+int verify_checksum(const char *sentence, size_t length) {
+    if (sentence[length - 3] != '*') {
+        return 0; // Invalid sentence format
+    }
 
-int main(void)
-{
-UNITY_BEGIN();
-RUN_TEST(test_checksum_over_examples);
-return UNITY_END();
+    unsigned char expected_checksum;
+    if (sscanf(sentence + length - 2, "%2hhx", &expected_checksum) != 1) {
+        return 0; // Invalid checksum format
+    }
+
+    unsigned char calculated_checksum = calculate_checksum(sentence);
+    return calculated_checksum == expected_checksum;
 }
+
+// Rest of your code remains the same
